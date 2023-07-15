f96d02c42b005415ab1ca8cddac7a7ee690a2620
Cho JeongTaek <cjt3591@gmail.com>
2023-07-15 18:09:44 +0900

    ✨ Feat :  라우터 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import MainPage from './pages/main';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './styles/GlobalStyle.ts';
+import RouteComponents from './constants/routes/routs.tsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <MainPage />
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <GlobalStyle />
+      <RouteComponents />
+    </Router>
+  </React.StrictMode>,
+);
