5285f5b4a0394de1a9e432a5d01e85f48c038798
Cho JeongTaek <cjt3591@gmail.com>
2023-07-16 19:16:14 +0900

    💄 Design:  실시간 상담 폼 퍼블리싱_PC

--- src/pages/Chats/style.ts
+++ src/pages/Chats/style.ts
@@ -85,73 +85,8 @@ export const ChatListContainer = styled.div`
   margin: 0 auto 2.5rem;
 `;
 
-export const ChatList = styled.ul`
+export const ChatLists = styled.ul`
   width: 100%;
   display: flex;
   flex-direction: column;
-
-  li {
-    padding: 2rem;
-    border-bottom: 1px solid #000;
-  }
-`;
-
-export const ListBox = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const ListContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-`;
-
-export const ListBtnBox = styled.button`
-  width: 10rem;
-  padding: 0.8rem;
-  border: 1px solid ${STYLE.mainColor};
-  font-size: 1.5rem;
-  font-weight: 400;
-  color: ${STYLE.mainColor};
-  background-color: #fff;
-  border-radius: 6px;
-
-  &:hover {
-    background-color: ${STYLE.mainColor};
-    color: #fff;
-  }
-`;
-
-export const ListContentBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-export const NameRateBox = styled.div`
-  display: flex;
-  gap: 1.2rem;
-  align-items: center;
-
-  p {
-    font-size: 1.8rem;
-    font-weight: 400;
-    color: ${STYLE.mainFontColor};
-  }
-`;
-
-export const ListDetail = styled.p`
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  width: 50rem;
-  font-size: 1.3rem;
-  font-weight: 400;
-  color: ${STYLE.subFontColor};
-  text-overflow: ellipsis;
-  overflow: hidden;
-  line-height: 1.4;
 `;
