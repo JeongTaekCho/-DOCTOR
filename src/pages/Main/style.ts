74c0d2ac8c2f168ded733d1959e66c544caaffca
YooSeungJe <dbtmdwp3562@gmail.com>
2023-07-19 01:21:45 +0900

    ✨ Feat :  메인페이지 구현

--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -2,5 +2,144 @@ import { styled } from 'styled-components';
 
 export const Wrap = styled.div`
   width: 100%;
-  height: 100vh;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  margin-top: -8%;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  filter: brightness(80%);
+`;
+
+export const Header = styled.h4`
+  font-size: 10rem;
+  color: white;
+  font-weight: bolder;
+  position: absolute;
+  top: 50%;
+  padding-left: 5%;
+  line-height: 130%;
+  color: transparent;
+  -webkit-text-stroke: 3px #fff;
+  font-family: 'ABeeZee', sans-serif;
+`;
+
+export const AiDiv = styled.div`
+  width: 100%;
+`;
+
+export const AiIntro = styled.div`
+  text-align: center;
+  width: 100%;
+  padding-top: 15%;
+`;
+
+export const IntroHeader = styled.h4`
+  font-weight: bolder;
+  font-size: 4rem;
+  line-height: 130%;
+`;
+
+export const AiPic = styled.div`
+  width: 100%;
+  text-align: center;
+  padding-top: 4%;
+  padding-bottom: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Pic = styled.img`
+  height: 40rem;
+  width: 40rem;
+  margin-right: 5rem;
+  border-radius: 5px;
+`;
+
+export const SubDiv = styled.div`
+  width: 100%;
+  height: 50rem;
+  background-color: rgb(245, 245, 245);
+  padding-top: 5%;
+`;
+
+export const SubHeader = styled.h4`
+  text-align: center;
+  font-size: 3rem;
+  font-weight: bold;
+  line-height: 130%;
+`;
+
+export const ContentDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 3%;
+`;
+
+export const Content = styled.div`
+  width: 55rem;
+  height: 7.5rem;
+  background-color: white;
+  margin-right: 5rem;
+  border-radius: 10px;
+  box-shadow: lightgrey 2px 2px;
+  padding: 1.6% 0 0 1%;
+`;
+
+export const ContentP = styled.p`
+  font-size: 2rem;
+  font-weight: bolder;
+`;
+
+export const ContentSpan1 = styled.span`
+  color: #667085;
+  font-size: 1.7rem;
+  font-weight: 500;
+  margin-left: 20%;
+`;
+
+export const ContentSpan2 = styled.span`
+  font-size: 1.7rem;
+  font-weight: 500;
+  margin-left: 23%;
+  color: #667085;
+`;
+
+export const ContentSpan3 = styled.span`
+  font-size: 1.7rem;
+  font-weight: 500;
+  margin-left: 25%;
+  color: #667085;
+`;
+
+export const ContentSpan4 = styled.span`
+  font-size: 1.7rem;
+  font-weight: 500;
+  margin-left: 20%;
+  color: #667085;
+`;
+
+export const EffectDiv = styled.div`
+  width: 100%;
+  height: 70rem;
+  padding: 7% 0 15% 15%;
+  line-height: 900%;
+`;
+
+export const EffectHeader = styled.h4`
+  font-size: 3rem;
+  font-weight: bolder;
+`;
+
+export const EffectP = styled.p`
+  font-size: 2.5rem;
+  color: #667085;
+  font-weight: bolder;
 `;
