d820a84cf4f6f3e1912e84e661d60f7a2b8277e6
한빈 김 <gksqls0507@gmail.com>
2023-07-24 02:27:58 +0900

    ✨ Feat : 동물병원찾기페이지-퍼블리싱

--- src/pages/Hospital/index.tsx
+++ src/pages/Hospital/index.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+/* eslint-disable quotes */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+import React, { useEffect } from 'react';
 import * as S from './style';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const HospitalPage = () => {
-    return (
-        <S.Wrap>
-            <S.Container>
-                <S.Title>Hospital</S.Title>
-            </S.Container>
-        </S.Wrap>
-        );
-    };
+  useEffect(() => {
+    const container = document.getElementById('map') as HTMLElement;
+
+    if (window.kakao && window.kakao.maps) {
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+      };
+      const map = new kakao.maps.Map(container, options);
+    }
+  }, []);
+
+  return (
+    <S.Wrap>
+      <S.HospitalIntro>
+        <S.MainTitle>DOGTOR 동물 병원 찾기</S.MainTitle>
+        <S.SubTitle>가까운 동물 병원을 확인하세요</S.SubTitle>
+      </S.HospitalIntro>
+      <S.MapContent>
+        <S.Map id="map"></S.Map>
+      </S.MapContent>
+    </S.Wrap>
+  );
+};
 
 export default HospitalPage;
--- src/pages/Hospital/style.ts
+++ src/pages/Hospital/style.ts
@@ -1,18 +1,45 @@
 import { styled } from 'styled-components';
 
 export const Wrap = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
-export const Container = styled.div`
-    width: 100%;
-    margin-top: -8%;
+// 동물 병원 페이지 소개 제목 wrap
+export const HospitalIntro = styled.div`
+  text-align: left;
+  width: 100%;
+  padding-left: 5%;
+`;
+// 'DOGTOR 동물 병원 찾기'
+export const MainTitle = styled.h4`
+  font-size: 3rem;
+  font-weight: bolder;
+  width: 100%;
+  color: #5429ff;
+`;
+// '가까운 동물 병원을 확인하세요'
+export const SubTitle = styled.h4`
+  width: 100%;
+  font-size: 2rem;
+  font-weight: bolder;
+  padding-top: 2%;
+  color: #344054;
 `;
 
-export const Header = styled.h4`
-    font-size: 9rem;
+// 카카오맵
+export const MapContent = styled.div`
+  width: 100%;
+  height: 50rem;
+  padding-left: 5%;
+  padding-right: 5%;
+  padding-top: 2%;
 `;
 
-export const Title = styled.h4`
-    font-size: 3rem;
+export const Map = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: gray;
+  border: #b4b9c5 0.1rem solid;
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0px 0px 5px 0px #667085;
 `;
