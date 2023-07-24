013dae0e2cb3343b7d03e10ada2ab7496686d675
Cho JeongTaek <cjt3591@gmail.com>
2023-07-24 16:49:26 +0900

    💄 Design:  푸터 퍼블리싱_MOBILE

--- src/components/commons/Footer/style.ts
+++ src/components/commons/Footer/style.ts
@@ -1,10 +1,15 @@
 import { styled } from 'styled-components';
+import { RESPONSIVE } from '../../../styles/responsive';
 
 export const Wrap = styled.div`
   width: 100%;
   padding: 8rem 0;
   margin-top: 10rem;
   border-top: 1px solid #eee;
+
+  @media ${RESPONSIVE.tablet} {
+    padding: 6rem 0;
+  }
 `;
 
 export const Container = styled.div`
@@ -12,10 +17,19 @@ export const Container = styled.div`
   margin: 0 auto;
   display: flex;
   gap: 6rem;
+
+  @media ${RESPONSIVE.tablet} {
+    flex-direction: column;
+    gap: 3rem;
+  }
 `;
 
 export const LogoBox = styled.div`
   width: 20rem;
+
+  @media ${RESPONSIVE.tablet} {
+    margin: 0 auto;
+  }
 
   img {
     width: 100%;
@@ -30,6 +44,12 @@ export const FooterContents = styled.div`
 
     &:last-child {
       margin-top: 2rem;
+    }
+  }
+
+  @media ${RESPONSIVE.tablet} {
+    p {
+      text-align: center;
     }
   }
 `;
