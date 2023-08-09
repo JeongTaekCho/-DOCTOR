b103b7a0c17712c1882d219a8eda95dea415e40b
한빈 김 <gksqls0507@gmail.com>
2023-08-10 00:23:20 +0900

    💄 Design:  관리자페이지-유저아이디일정길이이상자동스크롤(스크롤바숨김)

--- src/components/admin/LayoutAdmin/ListSet/UserInfoList.tsx
+++ src/components/admin/LayoutAdmin/ListSet/UserInfoList.tsx
@@ -208,6 +208,19 @@ const ReportPrifileId = styled.div`
   font-size: 1.4rem;
   font-weight: 600;
   color: #252733;
+  overflow-x: auto;
+  white-space: nowrap;
+  &::-webkit-scrollbar {
+    width: 0 !important;
+    height: 0 !important;
+    background: transparent;
+  }
+
+  /* Internet Explorer and Edge */
+  -ms-overflow-style: none;
+
+  /* Firefox */
+  scrollbar-width: none;
 `;
 //-------------------------------------
 const ReportComment = styled.p`
