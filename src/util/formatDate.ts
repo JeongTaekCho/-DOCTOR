b5c4f69e5810ffecb05ca1a0df5141ae1f7758ab
Cho JeongTaek <cjt3591@gmail.com>
2023-08-10 16:27:05 +0900

    🔨 Modify:  날짜 포맷 수정

--- src/util/formatDate.ts
+++ src/util/formatDate.ts
@@ -1,5 +1,5 @@
 export const formatDate = (dateString: any) => {
-  const date = new Date(dateString);
   // const options: any = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-  return date.toISOString().split('T')[0];
+  console.log(typeof dateString);
+  return dateString?.split('T')[0];
 };
