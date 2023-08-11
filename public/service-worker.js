ca262da769c5f2fbbfdb8c687cdbb605cf937788
Cho JeongTaek <cjt3591@gmail.com>
2023-08-11 23:37:09 +0900

    🔨 Modify:  PWA 수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,3 +29,22 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // 자동으로 설치 프롬프트 표시
+  deferredPrompt.prompt();
+
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
+  });
+});
